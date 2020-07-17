1ae328aacd34c5896b4319bb0c5f4b1ed3773b52
David Takazaki <davidmtakazaki@hotmail.com>
2020-07-17 15:03:26 -0300

    implements course module

--- simplemoc/simplemoc/settings.py
+++ simplemoc/simplemoc/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'simplemoc.core'
+    'simplemoc.core',
+    'simplemoc.courses'
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "simplemoc", "media")

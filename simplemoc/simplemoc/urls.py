d98e54788590572318d0bc893e5f0cec091ecd86
David Takazaki <davidmtakazaki@hotmail.com>
2020-07-17 15:26:15 -0300

    implements course view in frontend site

--- simplemoc/simplemoc/urls.py
+++ simplemoc/simplemoc/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(("simplemoc.core.urls", "core"), namespace="core"))
+    path('', include(("simplemoc.core.urls", "core"), namespace="core")),
+    path('cursos', include(("simplemoc.courses.urls", "course"), namespace="course"))
 ]

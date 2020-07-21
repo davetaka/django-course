eb96564be6eba4ed2cddfa4f0d1f40280da3b5a7
David Takazaki <davidmtakazaki@hotmail.com>
2020-07-21 20:38:59 -0300

    implements register functions

--- simplemoc/simplemoc/accounts/urls.py
+++ simplemoc/simplemoc/accounts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from django.contrib.auth.views import LoginView
+from . import views
 
 urlpatterns = [
-    path("entrar/", LoginView.as_view(template_name="accounts/login.html"), name="login")
+    path("entrar/", LoginView.as_view(template_name="accounts/login.html"), name="login"),
+    path("cadastre-se/", views.register, name="register")
 ]

e8a5827badae8de7c577cb12cee246d02395962a
David Takazaki <davidmtakazaki@hotmail.com>
2020-07-22 16:28:43 -0300

    add user dashboard

--- simplemoc/simplemoc/accounts/urls.py
+++ simplemoc/simplemoc/accounts/urls.py
@@ -3,6 +3,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 from . import views
 
 urlpatterns = [
+    path("dashboard/", views.dashboard, name="dashboard"),
     path("entrar/", LoginView.as_view(template_name="accounts/login.html"), name="login"),
     path("cadastre-se/", views.register, name="register"),
     path("sair/", LogoutView.as_view(next_page="core:home"), name="logout")

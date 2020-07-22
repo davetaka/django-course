4b22bfcca73cfb02a2360741583dc1f507ae9c83
David Takazaki <davidmtakazaki@hotmail.com>
2020-07-21 21:50:48 -0300

    implements logout action

--- simplemoc/simplemoc/accounts/urls.py
+++ simplemoc/simplemoc/accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from . import views
 
 urlpatterns = [
     path("entrar/", LoginView.as_view(template_name="accounts/login.html"), name="login"),
-    path("cadastre-se/", views.register, name="register")
+    path("cadastre-se/", views.register, name="register"),
+    path("sair/", LogoutView.as_view(next_page="core:home"), name="logout")
 ]

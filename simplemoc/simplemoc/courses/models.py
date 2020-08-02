913e1a7979a62c4ff0494d52e16d694ec42ffb9d
David Takazaki <davidmtakazaki@hotmail.com>
2020-08-02 16:56:13 -0300

    add comments and announcements structures

--- simplemoc/simplemoc/courses/models.py
+++ simplemoc/simplemoc/courses/models.py
@@ -83,3 +83,41 @@ class Enrollments(models.Model):
         verbose_name = "Inscrição"
         verbose_name_plural = "Inscrições"
         unique_together = (("user", "course"),)
+
+
+class Announcement(models.Model):
+    course = models.ForeignKey(Course, verbose_name="Curso", on_delete=models.CASCADE)
+    title = models.CharField("Titulo", max_length=100)
+    content = models.TextField("Conteúdo")
+
+    created_at = models.DateTimeField("Criado em", auto_now_add=True)
+    updated_at = models.DateTimeField("Atualizado em", auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Anúncio"
+        verbose_name_plural = "Anúncios"
+        ordering = ["-created_at"]
+
+
+class Comment(models.Model):
+
+    announcement = models.ForeignKey(
+        Announcement,
+        verbose_name="Anúncio",
+        related_name="comments",
+        on_delete=models.CASCADE
+    )
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name="usuário", on_delete=models.CASCADE)
+    comment = models.TextField("Comentário")
+
+    created_at = models.DateTimeField("Criado em", auto_now_add=True)
+    updated_at = models.DateTimeField("Atualizado em", auto_now=True)
+
+    class Meta:
+        verbose_name = "Comentário"
+        verbose_name_plural = "Comentários"
+        ordering = ["created_at"]

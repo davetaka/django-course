45489c26382aab1dbe2ea22f96883ac64c124c25
David Takazaki <davidmtakazaki@hotmail.com>
2020-08-02 21:17:15 -0300

    fix url control bug

--- simplemoc/simplemoc/forum/urls.py
+++ simplemoc/simplemoc/forum/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("tag/<str:tag>", views.index, name="index_tagged"),
     path("respostas/<int:pk>/correta", views.reply_correct, name="reply_correct"),
     path("respostas/<int:pk>/incorreta", views.reply_incorrect, name="reply_incorrect"),
-    path("tag/<str:slug>", views.index, name="thread"),
+    path("tag/<str:tag>", views.index, name="index_tagged"),
+    path("<str:slug>", views.thread, name="thread"),
 ]
